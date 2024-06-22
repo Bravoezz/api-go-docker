17dd4b6222e970d9896b5e409470e143d40255ee
Bravoezz <joelmrd1234@gmail.com>
2024-06-22 14:24:39 -0500

    add configs

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"github.com/Bravoezz/infra_srv_tic/cmd/api"
+	"github.com/Bravoezz/infra_srv_tic/cmd/config"
 	"log"
 )
 
 func main() {
-	server := api.NewApiServer(":8000", nil)
+	var port string
+	if config.GetEnv("PORT") == "" {
+		port = "8000"
+	} else {
+		port = config.GetEnv("PORT")
+	}
+
+	server := api.NewApiServer(":"+port, nil)
 
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Bravoezz/infra_srv_tic
 
 go 1.22.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
